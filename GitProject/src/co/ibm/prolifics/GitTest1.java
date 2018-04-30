238b41e99f4b517aeb07acd18a2a9df4f53cb65c
BKarampuri <BKarampuri@DLF-8F-D188.ad.prolifics.com>
2018-04-30 15:11:28 +0530

    first commit of gitproject

---
+++ GitProject/src/co/ibm/prolifics/GitTest1.java
@@ -0,0 +1,16 @@
+package co.ibm.prolifics;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+		GitTest1 gitTest1=new GitTest1();
+		gitTest1.test();
+	}
+	
+	public void test()
+	{
+		System.out.println("hello welcome to git project");
+	}
+
+}
